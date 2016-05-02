66080d7250afd11af97da8e3c39585b64b644e96
COMSYS <comsys@comsys.solutions>
2016-05-02 16:11:00 +1000

    provider hosted converted to asp.net mvc example - how to 1

--- SPProviderHostedAddInWeb/Controllers/HomeController.cs
+++ SPProviderHostedAddInWeb/Controllers/HomeController.cs
@@ -5,33 +5,149 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Microsoft.IdentityModel.S2S.Tokens;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+using SPProviderHostedAddInWeb.Models;
+
 namespace SPProviderHostedAddInWeb.Controllers
 {
     public class HomeController : Controller
     {
+
         [SharePointContextFilter]
         public ActionResult Index()
         {
             User spUser = null;
+            SPModel spModel = new SPModel();
+
+            // factor out into property and put in session
+            string contextTokenString = TokenHelper.GetContextTokenFromRequest(Request);
+
+            if (contextTokenString != null)
+            {
+                spModel.ContextToken = TokenHelper.ReadAndValidateContextToken(contextTokenString, Request.Url.Authority);
+                spModel.SharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+                spModel.AccessToken = TokenHelper.GetAccessToken(spModel.ContextToken, spModel.SharepointUrl.Authority).AccessToken;
+
+               // For simplicity, this sample assigns the access token to the button's CommandArgument property. 
+               // In a production add-in, this would not be secure. The access token should be cached on the server-side.
+               // CSOM.CommandArgument = accessToken; - change this to an action link Post
+            }
+
 
             var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
 
-            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            using (spModel.SPClientContext = spContext.CreateUserClientContextForSPHost())
             {
-                if (clientContext != null)
+                if (spModel.SPClientContext != null)
                 {
-                    spUser = clientContext.Web.CurrentUser;
+                    //spModel.SPClientContext.
 
-                    clientContext.Load(spUser, user => user.Title);
-
-                    clientContext.ExecuteQuery();
-
+                    spUser = spModel.SPClientContext.Web.CurrentUser;
+                    spModel.SPClientContext.Load(spUser, user => user.Title);
+                    spModel.SPClientContext.ExecuteQuery();
                     ViewBag.UserName = spUser.Title;
                 }
             }
 
-            return View();
+            return View(spModel);
         }
+
+        // This method retrieves information about the host web by using the CSOM. Could be in a Data Layer sep component - but not today
+        private SPModel RetrieveWithCSOM(SPModel spModel)
+        {
+
+            //if (IsPostBack)
+            //{
+            //    sharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+            //}
+
+            //spModel.SPClientContext = TokenHelper.GetClientContextWithAccessToken(sharepointUrl.ToString(), accessToken);
+
+            // Load the properties for the web object.
+            Web web = spModel.SPClientContext.Web;
+            spModel.SPClientContext.Load(web);
+            spModel.SPClientContext.ExecuteQuery();
+
+            // Get the site name.
+            spModel.SiteName = web.Title;
+
+            // Get the current user.
+            spModel.SPClientContext.Load(web.CurrentUser);
+            spModel.SPClientContext.ExecuteQuery();
+            spModel.CurrentUser = spModel.SPClientContext.Web.CurrentUser.LoginName;
+
+            // Load the lists from the Web object.
+            ListCollection lists = web.Lists;
+            spModel.SPClientContext.Load<ListCollection>(lists);
+            spModel.SPClientContext.ExecuteQuery();
+
+            // Load the current users from the Web object.
+            UserCollection users = web.SiteUsers;
+            spModel.SPClientContext.Load<UserCollection>(users);
+            spModel.SPClientContext.ExecuteQuery();
+
+            foreach (User siteUser in users)
+            {
+                spModel.ListOfUsers.Add(siteUser.LoginName);
+            }
+
+
+            foreach (List list in lists)
+            {
+                spModel.ListOfLists.Add(list.Title);
+            }
+
+            return spModel;
+        }
+
+        public ActionResult CSOM_Click()
+        {
+            ViewBag.Message = "CSOM Clicked View";
+
+            User spUser = null;
+            SPModel spModel = new SPModel();
+
+
+            string contextTokenString = TokenHelper.GetContextTokenFromRequest(Request);
+
+            if (contextTokenString != null)
+            {
+                spModel.ContextToken = TokenHelper.ReadAndValidateContextToken(contextTokenString, Request.Url.Authority);
+
+                spModel.SharepointUrl = new Uri(Request.QueryString["SPHostUrl"]);
+                spModel.AccessToken = TokenHelper.GetAccessToken(spModel.ContextToken, spModel.SharepointUrl.Authority).AccessToken;
+
+                // For simplicity, this sample assigns the access token to the button's CommandArgument property. 
+                // In a production add-in, this would not be secure. The access token should be cached on the server-side.
+                // CSOM.CommandArgument = accessToken; - change this to an action link Post
+            }
+
+
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+
+            using (spModel.SPClientContext = spContext.CreateUserClientContextForSPHost())
+            {
+                if (spModel.SPClientContext != null)
+                {
+                    //spModel.SPClientContext.
+
+                    spUser = spModel.SPClientContext.Web.CurrentUser;
+                    spModel.SPClientContext.Load(spUser, user => user.Title);
+                    spModel.SPClientContext.ExecuteQuery();
+                    ViewBag.UserName = spUser.Title;
+                }
+            }
+
+            RetrieveWithCSOM(spModel);
+
+            return View(spModel);           
+        }
+
+
 
         public ActionResult About()
         {
---
+++ SPProviderHostedAddInWeb/Models/SPModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.SharePoint.Client;
+
+namespace SPProviderHostedAddInWeb.Models
+{
+    public class SPModel
+    {
+        private SharePointContextToken contextToken;
+        private string accessToken;
+        private Uri sharepointUrl;
+        private string siteName;
+        private string currentUser;
+        private List<string> listOfUsers = new List<string>();
+        private List<string> listOfLists = new List<string>();
+
+        private ClientContext spClientContext;
+
+        public SPModel()
+        {
+
+        }
+
+        public Uri SharepointUrl
+        {
+            get
+            {
+                return sharepointUrl;
+            }
+
+            set
+            {
+                sharepointUrl = value;
+            }
+        }
+
+        public SharePointContextToken ContextToken
+        {
+            get
+            {
+                return contextToken;
+            }
+
+            set
+            {
+                contextToken = value;
+            }
+        }
+
+        public string AccessToken
+        {
+            get
+            {
+                return accessToken;
+            }
+
+            set
+            {
+                accessToken = value;
+            }
+        }
+
+        public string SiteName
+        {
+            get
+            {
+                return siteName;
+            }
+
+            set
+            {
+                siteName = value;
+            }
+        }
+
+        public string CurrentUser
+        {
+            get
+            {
+                return currentUser;
+            }
+
+            set
+            {
+                currentUser = value;
+            }
+        }
+
+        public List<string> ListOfUsers
+        {
+            get
+            {
+                return listOfUsers;
+            }
+
+            set
+            {
+                listOfUsers = value;
+            }
+        }
+
+        public List<string> ListOfLists
+        {
+            get
+            {
+                return listOfLists;
+            }
+
+            set
+            {
+                listOfLists = value;
+            }
+        }
+
+        public ClientContext SPClientContext
+        {
+            get
+            {
+                return spClientContext;
+            }
+
+            set
+            {
+                spClientContext = value;
+            }
+        }
+    }
+}
